6b7081d9394411bf6cf9846632f0464e82a85a5d
Stephen Fluin <stephen.fluin@gmail.com>
2018-06-29 15:52:18 -0700

    scaffold app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ChatComponent } from './chat/chat.component';
+import { StatsComponent } from './stats/stats.component';
+import { RulesComponent } from './rules/rules.component';
+import { LogoutComponent } from './logout/logout.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavigationComponent,
+        HomeComponent,
+        ProfileComponent,
+        ChatComponent,
+        StatsComponent,
+        RulesComponent,
+        LogoutComponent,
+    ],
+    imports: [
+        RouterModule.forRoot([
+            { path: '', component: HomeComponent },
+            { path: 'profile', component: ProfileComponent },
+            { path: 'chat', component: ChatComponent },
+            { path: 'stats', component: StatsComponent },
+            { path: 'rules', component: RulesComponent },
+            { path: 'logout', component: LogoutComponent },
+        ]),
+        BrowserModule,
+        BrowserAnimationsModule,
+        LayoutModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatListModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+    selector: 'app-navigation',
+    templateUrl: './navigation.component.html',
+})
+export class NavigationComponent {
+    isHandset$: Observable<boolean> = this.breakpointObserver
+        .observe(Breakpoints.Handset)
+        .pipe(map(result => result.matches));
+
+    constructor(private breakpointObserver: BreakpointObserver) {}
+}
